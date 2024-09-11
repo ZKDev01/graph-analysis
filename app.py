e51f4d73177d44a5596a95e5f1c715988e9303ce
ZKDev01 <rdromaguera2001@gmail.com>
2024-09-11 18:04:59 -0400

    deploy with streamlit

--- app.py
+++ app.py
@@ -3,68 +3,146 @@ import matplotlib.pyplot as plt
 
 import streamlit as st
 
-def main ( ) -> None:
+from src.words import (
+  MOVIE_GENRE_LIST,
+  MUSIC_GENRE_LIST,
+  NODE_TYPES
+)
 
-  st.write ( '# TESTING' )
+from src.data_faker import (
+  generator_name_list,
+  generator_tokens_unique_list,
+  generator_nodes
+)
 
-  G = nx.Graph()
+from src.random_models import (
+  model_ER,
+  model_BA,
+  model_WS
+)
 
-  nodes = [ i for i in range(1, 100) ]
-  edges = [ 
-    ( 1, 2 ),
-    ( 1, 3 ),
-    ( 2, 3 ),
-    ( 2, 5 ),
-    ( 4, 5 ),
-    ( 5, 6 ),
-    ( 5, 7 ),
-    ( 6, 7 )
-  ]
+from src.graph import (
+  Node, 
+  DynamicGraph,
+  MultiplexGraph,
+  TemporalGraph
+)
 
-  G.add_nodes_from( nodes )
-  G.add_edges_from( edges )
+from src.dynamic import (
+  Dynamic
+)
 
-  # Graficar el grafo
-  fig, ax = plt.subplots ( figsize=(25,25) )
-  pos = nx.circular_layout ( G )
+from src.diffusion_functions import (
+  Base_DiffusionFunction,
+  Probabilistic_DiffusionFunction,
+  Metadata_DiffusionFunction,
+  MostPopular_DiffusionFunction
+)
 
-  nx.draw(G, with_labels=True, pos=pos, ax=ax)
-  # plt.show()
+from src.simulation import (
+  InformationDiffusion_Simulator
+)
+
+# region: AUXILIAR
+
+def flattern_dict_pairs ( adj_list: dict[ int, list[int] ] ) -> list[ (int, int) ]:
+  result = [ ]
+  for key, values in adj_list.items ( ):
+    for value in values:
+      result.append ( (key, value) )
+  return result
+
+def get_nx_graph_from_dynamic_graph ( dynamic: DynamicGraph ) -> nx.Graph:
+  graph = nx.Graph ( )
   
+  nodes = [ i for i in dynamic.ids ]
+  edges = flattern_dict_pairs ( dynamic.adj_list )
+
+  graph.add_nodes_from( nodes )
+  graph.add_edges_from( edges )
+  
+  return graph
+
+# endregion: AUXILIAR
+
+# region: DISPLAY-IN-STREAMLIT
+
+def display_dynamic_graph ( dynamic_graph: DynamicGraph ) -> None:
+  nx_graph = get_nx_graph_from_dynamic_graph ( dynamic=dynamic_graph )
+  fig, ax = plt.subplots ( figsize=(25,25) )
+  pos = nx.circular_layout ( G=nx_graph )
+
+  nx.draw ( nx_graph, with_labels=True, pos=pos, ax=ax )
   st.pyplot ( fig=fig )
 
+# endregion: DISPLAY-IN-STREAMLIT
+
+
+
+def deploy ( ) -> None:
+  st.title ( 'Proyect: Graph Analysis' )
+
+  n_nodes = st.sidebar.number_input ( 'Select a number of nodes', min_value=0, max_value=100 )
+  p_model_ER = st.sidebar.number_input ( 'Select a value for model-ER', min_value=0.0, max_value=1.0 )
+  step_simulation = st.sidebar.number_input ( 'Select a number of simulation step', min_value=1, max_value=5 )
+
+  dynamic_graph = None
+
+  with st.form ( 'main-form' ):
+    
+    if n_nodes > 0 and dynamic_graph == None:
+      nodes = generator_nodes ( 
+        n=n_nodes,
+        node_types=NODE_TYPES,
+        preferences={
+          'movie':MOVIE_GENRE_LIST,
+          'music':MUSIC_GENRE_LIST
+        }
+      )
+      dynamic_graph = DynamicGraph ( nodes=nodes )
+      
+      model_ER ( graph=dynamic_graph, p=p_model_ER )
+
+      st.form_submit_button ( 'Submit Dynamic Graph' )
+
+  if dynamic_graph != None:
+    
+    multiplex_graph = MultiplexGraph ( )
+    multiplex_graph.add_graph ( 'model-ER', dynamic_graph )
+
+    diffusion_functions = { }
+    for i in range ( n_nodes ):
+      diffusion_functions[ i ] = Probabilistic_DiffusionFunction ( 
+        dynamic_graph.nodes[i], 
+        p=0.5
+      )
+    
+    root = 0
+    select_layer = 'model-ER'
+    f_dynamic = Dynamic ( 'add-node' )
+    p_dynamic = 0.5
+
+    simulation = InformationDiffusion_Simulator ( 
+      graph=multiplex_graph,
+      root=root,
+      p=p_dynamic
+    )
+    result = simulation.simulate (
+      layer=select_layer,
+      diffusion_functions=diffusion_functions,
+      dynamic=f_dynamic,
+      step=step_simulation
+    )
+
+    temporal: TemporalGraph = result [ 'Temporal Graph' ]
+    for i, graph in enumerate( temporal.graphs ):
+      st.write ( f'Step Simulation { i }' )
+      display_dynamic_graph ( dynamic_graph=graph )
+
+
 if __name__ == '__main__':
-  main ( )
+  deploy ( )
 
 
 
 
-
-""" 
-from diffusion import (
-  InformationDiffusion_Simulator,
-  DynamicGraph
-)
-from src.random_models import ( 
-  erdos_renyi_model,
-)
-
-import random
-
-def test_1 ( ) -> None:
-  n = 100
-  root = random.randint ( 0, n-1 )
-  step = 100
-
-  graph = DynamicGraph ( n )
-  graph = erdos_renyi_model ( graph, p=0.5 )
-
-  simulator = InformationDiffusion_Simulator ( graph, root )
-
-  for _ in range ( step ):
-    simulator.simulate_step ( )
-
-  print ( f"Nodos con Informacion: { len( simulator.reported_nodes ) }" )
-  print ( f'Numero de nodos finales: { graph.n }' )
-
-"""
