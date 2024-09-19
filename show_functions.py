c13f4f3ae2888d6cf8b01caf359baff30b5a917d
ZKDev01 <rdromaguera2001@gmail.com>
2024-09-19 09:28:31 -0400

    ignore testing. def functions for display

---
+++ show_functions.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pandas as pd 
+
+# defining types
+from pandas import DataFrame
+from src.graph import Vertex
+from typing import List, Set, Dict, Any
+
+def show_in_streamlit_generated_vertexs_like_a_dataframe ( vertexs: List[ Vertex ] ) -> None:
+  # transform vertex in ( name, type, [ metadatas ] ) => ( name, type, like-movies, like-music )
+
+  pass
+
